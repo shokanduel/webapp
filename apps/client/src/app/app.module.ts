681ba3772018ba1264c84c38a7189dc15bdfc12e
Ethan Massie <ethan@emassie.dev>
2021-05-06 12:48:28 -0500

    added the root router module

--- apps/client/src/app/app.module.ts
+++ apps/client/src/app/app.module.ts
@@ -3,10 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
